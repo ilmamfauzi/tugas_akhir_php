84eaa33926769a136521bd95054e9b0d59749b4d
Ilmam Fauzi <ilmamfauzi@gmail.com>
2021-10-11 13:19:08 +0700

    setting database

--- database/migrations/2021_10_11_061455_tbl_biodata.php
+++ database/migrations/2021_10_11_061455_tbl_biodata.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class TblBiodata extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('tbl_biodata', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('no_hp');
+            $table->string('alamat');
+            $table->string('hobi');
+            $table->string('foto');
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 }
